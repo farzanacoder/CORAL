65546dd510be6836f89f07fb117e5107fafe4507
farzanacoder <farzanaashif10@gmail.com>
2025-08-06 00:23:46 +0600

    react and tailind setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+         <h1 class="text-3xl text-amber-300 font-bold underline">
+    Hello world!
+  </h1>
+    </div>
+  )
+}
+
+export default App

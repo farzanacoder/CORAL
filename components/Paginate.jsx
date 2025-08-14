6345b152254ff98edf070c28f6170b440be89394
farzanacoder <farzanaashif10@gmail.com>
2025-08-14 11:53:37 +0600

    Pagination with API

--- components/Paginate.jsx
+++ components/Paginate.jsx
@@ -1,29 +1,42 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import ReactPaginate from 'react-paginate';
-import Bag from '../src/assets/14.png'
-import Card from './Card';
-
-const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import ReactPaginate from "react-paginate";
+import Bag from "../src/assets/14.png";
+import Card from "./Card";
+import axios from "axios";
 
 function Items({ currentItems }) {
   return (
     <>
-      <div className='flex flex-wrap gap-6.5'>
+      <div className="flex flex-wrap gap-6.5">
         {currentItems &&
-        currentItems.map(() => (
-          <div>
-            <Card title='Nike Sports Futura Luxe' category='bag' price='$ 12.99' src={Bag}/>
-          </div>
-        ))}
+          currentItems.map((item) => (
+            <div>
+              <Card
+                title={item.title}
+                category="bag"
+                price={item.price}
+                src={item.thumbnail}
+              />
+            </div>
+          ))}
       </div>
     </>
   );
 }
 
 function Paginate({ itemsPerPage }) {
-
   const [itemOffset, setItemOffset] = useState(0);
+  let [items, setItems] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      let data = await axios.get("https://dummyjson.com/products");
+      setItems(data.data.products);
+    }
+
+    fetchData();
+  }, []);
 
   const endOffset = itemOffset + itemsPerPage;
   console.log(`Loading items from ${itemOffset} to ${endOffset}`);
@@ -49,10 +62,12 @@ function Paginate({ itemsPerPage }) {
         pageCount={pageCount}
         previousLabel=""
         renderOnZeroPageCount={null}
+        containerClassName="flex gap-2 mt-10 cursor-pointer"
+        activeClassName="bg-blue-500 text-white px-3 py-1 rounded"
+        pageClassName="px-3 py-1 border rounded"
       />
     </>
   );
 }
 
-
-export default Paginate
+export default Paginate;
